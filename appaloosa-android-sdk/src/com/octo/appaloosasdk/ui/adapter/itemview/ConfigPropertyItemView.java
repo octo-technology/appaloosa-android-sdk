0bdcb8d3e36e284a362f0e095a675ba8b13756b7
Reda <mmouhamed@octo.com>
2013-07-30 14:42:30 +0200

    delete special release conf

--- appaloosa-android-sdk/src/com/octo/appaloosasdk/ui/adapter/itemview/ConfigPropertyItemView.java
+++
@@ -1,9 +0,0 @@
-package com.octo.appaloosasdk.ui.adapter.itemview;
-
-import android.view.LayoutInflater;
-import android.view.View;
-
-public interface ConfigPropertyItemView {
-    public int getViewType();
-    public View getView(LayoutInflater inflater, View convertView);
-}
