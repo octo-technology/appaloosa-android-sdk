4865d54ee3437d99365de4f353479de4c6fca21d
Christopher Parola <cparola@octo.com>
2013-09-10 11:59:39 +0200

    Prod url in webservices

--- src/com/octo/appaloosasdk/webservices/WebServices.java
+++ src/com/octo/appaloosasdk/webservices/WebServices.java
@@ -26,8 +26,7 @@ public class WebServices {
 	// CONSTANTS
 	// ============================================================================================
 	/** base url of appaloosa web services */
-//	public static final String WEBSERVICES_BASE_URL = "https://www.appaloosa-store.com/";
-	public static final String WEBSERVICES_BASE_URL = "http://appaloosa-int.herokuapp.com/";
+	public static final String WEBSERVICES_BASE_URL = "https://www.appaloosa-store.com/";
 
 	/** Timeout when calling a web service (in ms). */
 	private static final int WEBSERVICES_TIMEOUT = 30000;
